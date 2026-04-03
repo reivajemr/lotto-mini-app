7f9fe7758cf43bd44c31dcb58f2c08858755e7a4
reivajemr <reivajemr@gmail.com>
2026-04-02 20:17:36 -0400

    are2

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import App from './App';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const MANIFEST_URL = `${window.location.origin}/tonconnect-manifest.json`;
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <TonConnectUIProvider
-      manifestUrl={MANIFEST_URL}
-      actionsConfiguration={{
-        returnStrategy: 'back',
-      }}
-      walletsListConfiguration={{
-        // Excluir telegram-wallet (SCW) que causa el error en móvil
-        // Solo mostrar wallets con bridge confiable
-        excludeWallets: ['telegram-wallet'],
-      }}
-    >
-      <App />
-    </TonConnectUIProvider>
-  </React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
